9965a31cfa79c900f285fea7f3ce471e67aa5dbe
Ateto1202 <tonysu1204@gmail.com>
2024-11-04 22:28:36 +0800

    Create the parsers to transfer entity and domain model

--- internal/repository/chat_repository.go
+++ internal/repository/chat_repository.go
@@ -10,9 +10,9 @@ import (
 )
 
 type ChatRepository interface {
-	Save(chatID string, name string, membersID []string, t time.Time) (*entity.Chat, error)
+	Save(chatID string, name string, membersID []string, t time.Time) (*domain.Chat, error)
 	GetByID(id string) (*domain.Chat, error)
-	UpdByID(id string) error
+	UpdContentsByID(chat *domain.Chat) (*domain.Chat, error)
 }
 
 type chatRepository struct {
@@ -23,50 +23,85 @@ func NewChatRepository(db *gorm.DB) ChatRepository {
 	return &chatRepository{db}
 }
 
-func (r *chatRepository) Save(chatID string, name string, membersID []string, t time.Time) (*entity.Chat, error) {
-	members, err := strSerialize(membersID)
-	if err != nil {
-		return nil, err
-	}
-	chat := &entity.Chat{
+func (r *chatRepository) Save(chatID string, name string, membersID []string, t time.Time) (*domain.Chat, error) {
+	chatModel := &domain.Chat{
 		ID:       chatID,
 		Name:     name,
-		Members:  members,
-		Contents: "[]",
+		Members:  membersID,
+		Contents: []string{},
 		CreateAt: t,
 	}
-	err = r.db.Create(chat).Error
+	chatEntity, err := parseToEntity(chatModel)
 	if err != nil {
 		return nil, err
 	}
-	return chat, nil
+	err = r.db.Create(chatEntity).Error
+	if err != nil {
+		return nil, err
+	}
+	return chatModel, nil
 }
 
 func (r *chatRepository) GetByID(chatID string) (*domain.Chat, error) {
-	var chat entity.Chat
+	var chat *entity.Chat
 	err := r.db.Where("id = ?", chatID).Order("id").First(&chat).Error
 	if err != nil {
 		return nil, err
 	}
-	members, err := strUnserialize(chat.Members)
+	chatModel, err := parseToModel(chat)
 	if err != nil {
 		return nil, err
 	}
-	contents, err := strUnserialize(chat.Contents)
-	if err != nil {
-		return nil, err
-	}
-	model := &domain.Chat{
-		ID:       chat.ID,
-		Members:  members,
-		Contents: contents,
-		CreateAt: chat.CreateAt,
-	}
-	return model, err
+	return chatModel, nil
 }
 
-func (r *chatRepository) UpdByID(id string) error {
-	return nil
+func (r *chatRepository) UpdContentsByID(chat *domain.Chat) (*domain.Chat, error) {
+	chatEntity, err := parseToEntity(chat)
+	if err != nil {
+		return nil, err
+	}
+	if err := r.db.Model(chatEntity).Update("Contents", chatEntity.Contents).Error; err != nil {
+		return nil, err
+	}
+	return r.GetByID(chat.ID)
+}
+
+func parseToEntity(chat *domain.Chat) (*entity.Chat, error) {
+	contentsStr, err := strSerialize(chat.Contents)
+	if err != nil {
+		return nil, err
+	}
+	membersStr, err := strSerialize(chat.Members)
+	if err != nil {
+		return nil, err
+	}
+	chatEntity := &entity.Chat{
+		ID:       chat.ID,
+		Name:     chat.Name,
+		Members:  membersStr,
+		Contents: contentsStr,
+		CreateAt: chat.CreateAt,
+	}
+	return chatEntity, nil
+}
+
+func parseToModel(chat *entity.Chat) (*domain.Chat, error) {
+	contentsData, err := strUnserialize(chat.Contents)
+	if err != nil {
+		return nil, err
+	}
+	membersData, err := strUnserialize(chat.Members)
+	if err != nil {
+		return nil, err
+	}
+	chatModel := &domain.Chat{
+		ID:       chat.ID,
+		Name:     chat.Name,
+		Members:  membersData,
+		Contents: contentsData,
+		CreateAt: chat.CreateAt,
+	}
+	return chatModel, nil
 }
 
 func strSerialize(sa []string) (string, error) {
